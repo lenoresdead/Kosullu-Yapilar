9d8da24b314134d67b11865b434a22bc7888361e
goxcen <yilmazgokcen28@gmail.com>
2024-11-28 15:43:04 +0300

    Update kartbilgileri.py

--- kartbilgileri.py
+++ kartbilgileri.py
@@ -11,27 +11,4 @@ if(kartbilgileri["kart no"]==kartno and kartbilgileri["cvv"]==cvv and kartbilgil
 else:
     print("islem onaylanamamistir,lutfen tekrar deneyiniz.")
 
-
-
-a=int(input("bir sayi giriniz"))
-b=int(input("bir sayi giriniz"))
-c=int(input("bir sayi giriniz"))
-if(a>b and a>c):
-    print("a sayisi en buyuktur")
-elif(b>a and b>c):
-    print("b sayisi en buyuktur")
-elif(c>a and c>b):
-    print("c sayisi en buyuktur")
-else:
-    print("girilen sayilarin arasinda en buyuk olan yoktur")
-
-
-
-yas=int(input("yasiniz:"))
-if(yas>=18):
-  print("araba ve motor ehliyeti alabilirsiniz.")
-elif(yas>=15):
-  print("motor ehliyeti alabilirsiniz.")
-else:
- print("hicbir ehliyeti alamazsiniz.")
   
