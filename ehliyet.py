fa0ffc533292694a8cb883cd59164e35c11d8d45
goxcen <yilmazgokcen28@gmail.com>
2024-11-28 15:42:43 +0300

    Create ehliyet.py

---
+++ ehliyet.py
@@ -0,0 +1,7 @@
+yas=int(input("yasiniz:"))
+if(yas>=18):
+  print("araba ve motor ehliyeti alabilirsiniz.")
+elif(yas>=15):
+  print("motor ehliyeti alabilirsiniz.")
+else:
+ print("hicbir ehliyeti alamazsiniz.")
